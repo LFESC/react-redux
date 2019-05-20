f28ce1291a757d18df22a856944e88e1f984f41f
郑聪 <931745526@qq.com>
2019-05-20 16:06:20 +0800

    first commit

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -22,24 +22,34 @@ class Provider extends Component {
   }
 
   componentDidMount() {
+    // TODO: 没搞懂哪里会用到_isMounted
     this._isMounted = true
 
+    // 初始化subscription，具体见trySubscribe方法
     this.state.subscription.trySubscribe()
 
+    // 如果构造函数里面初始state和componentDidMount里面的state不一致则触发所有listeners
+    // 暂时没想到为啥会不一样，可能是为了容错吧
     if (this.previousState !== this.props.store.getState()) {
       this.state.subscription.notifyNestedSubs()
     }
   }
 
   componentWillUnmount() {
+    // TODO: 没搞懂哪里会设置unsubscribe
     if (this.unsubscribe) this.unsubscribe()
 
+    // 卸载监听器
     this.state.subscription.tryUnsubscribe()
 
     this._isMounted = false
   }
 
   componentDidUpdate(prevProps) {
+    // 如果Provider发生了更新并且是由于store发生变化（这种情况应该不常见吧？）
+    // 卸载监听器
+    // 再根据新的store创建订阅者
+    // 后三行感觉和构造函数里面是重复的，可以提取成公共方法
     if (this.props.store !== prevProps.store) {
       this.state.subscription.tryUnsubscribe()
       const subscription = new Subscription(this.props.store)
@@ -48,11 +58,14 @@ class Provider extends Component {
     }
   }
 
+  // 触发订阅者里面的监听器
+  // 触发条件是store执行action修改了state后
   notifySubscribers() {
     this.state.subscription.notifyNestedSubs()
   }
 
   render() {
+    // 如果不传自定义的context就用默认的
     const Context = this.props.context || ReactReduxContext
 
     return (
--- src/utils/Subscription.js
+++ src/utils/Subscription.js
@@ -3,14 +3,21 @@ import { getBatch } from './batch'
 // encapsulates the subscription logic for connecting a component to the redux store, as
 // well as nesting subscriptions of descendant components, so that we can ensure the
 // ancestor components re-render before descendants
+// 封装订阅逻辑，用于将组件连接到redux存储
+// 以及嵌套订阅的后代组件，以便我们可以确保
+// 祖先组件在后代组件之前重新呈现
 
 const CLEARED = null
 const nullListeners = { notify() {} }
 
+// 创建了一个监听器的集合
 function createListenerCollection() {
   const batch = getBatch()
   // the current/next pattern is copied from redux's createStore code.
   // TODO: refactor+expose that code to be reusable here?
+  // current/next模式是从redux的createStore代码中复制的。
+  // TODO:重构+公开代码以便在这里重用?
+  // TODO: 不是很明白为啥要设置两个变量保存listeners
   let current = []
   let next = []
 
@@ -20,6 +27,7 @@ function createListenerCollection() {
       current = CLEARED
     },
 
+    // 执行所有的listeners
     notify() {
       const listeners = (current = next)
       batch(() => {
@@ -33,6 +41,7 @@ function createListenerCollection() {
       return next
     },
 
+    // 类似redux的subscribe，添加listener，并返回卸载方法
     subscribe(listener) {
       let isSubscribed = true
       if (next === current) next = current.slice()
@@ -59,35 +68,44 @@ export default class Subscription {
     this.handleChangeWrapper = this.handleChangeWrapper.bind(this)
   }
 
+  // 添加嵌套的listener
+  // 并且返回卸载方法
   addNestedSub(listener) {
     this.trySubscribe()
     return this.listeners.subscribe(listener)
   }
 
+  // 执行所有listeners
   notifyNestedSubs() {
     this.listeners.notify()
   }
 
+  // 当store变化时触发的方法
   handleChangeWrapper() {
     if (this.onStateChange) {
       this.onStateChange()
     }
   }
 
+  // 判断是否启动了订阅
   isSubscribed() {
     return Boolean(this.unsubscribe)
   }
 
+  // 初始化的功能
   trySubscribe() {
     if (!this.unsubscribe) {
       this.unsubscribe = this.parentSub
         ? this.parentSub.addNestedSub(this.handleChangeWrapper)
-        : this.store.subscribe(this.handleChangeWrapper)
+        : this.store.subscribe(this.handleChangeWrapper) // 给store上面添加了一个观察者，当store改变会执行所有listeners
 
       this.listeners = createListenerCollection()
     }
   }
 
+  // 卸载监听器，包含两个
+  // 1. 对redux store的监听
+  // 2. 内部添加的listeners
   tryUnsubscribe() {
     if (this.unsubscribe) {
       this.unsubscribe()
